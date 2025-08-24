25a885364204b4daedc2ff81e90f9771b5e9e1c5
Rudi Timmmermans <rudi.timmermans@mailfence.com>
2025-08-24 06:21:53 +0200

    Fix Dutch translation

--- lib/l10n/app_localizations_nl.dart
+++ lib/l10n/app_localizations_nl.dart
@@ -168,7 +168,7 @@ class AppLocalizationsNl extends AppLocalizations {
   String get removeAppsFromFolder => 'Apps verwijderen uit map';
 
   @override
-  String get reorderAppsInFolder => 'Apps opnieuw ordenen';
+  String get reorderAppsInFolder => 'Map apps opnieuw ordenen';
 
   @override
   String get noFolders => 'Nog geen mappen gemaakt';
